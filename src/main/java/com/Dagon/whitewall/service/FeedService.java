bb62aa3752865057f8b7dcfa76b57e4557967f2a
Dagon <31436836+Dagon0577@users.noreply.github.com>
2019-01-31 17:35:08 +0800

    day9 Timeline与新鲜事，推拉模式下的Feed流

---
+++ src/main/java/com/Dagon/whitewall/service/FeedService.java
@@ -0,0 +1,27 @@
+package com.Dagon.whitewall.service;
+
+import com.Dagon.whitewall.dao.FeedDAO;
+import com.Dagon.whitewall.model.Feed;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FeedService {
+    @Autowired
+    FeedDAO feedDAO;
+
+    public List<Feed> getUserFeeds(int maxId, List<Integer> userIds, int count){
+        return feedDAO.selectUserFeeds(maxId, userIds, count);
+    }
+
+    public boolean addFeed(Feed feed){
+        feedDAO.addFeed(feed);
+        return feed.getId() > 0;
+    }
+
+    public Feed getById(int id){
+        return feedDAO.getFeedById(id);
+    }
+}
--- src/main/java/com/Dagon/whitewall/util/RedisKeyUtil.java
+++ src/main/java/com/Dagon/whitewall/util/RedisKeyUtil.java
@@ -10,6 +10,7 @@ public class RedisKeyUtil {
     private static String BIZ_FOLLOWER = "FOLLOWER";
     //关注对象
     private static String BIZ_FOLLOWEE = "FOLLOWEE";
+    private static String BIZ_TIMELINE = "TIMELINE";
 
     public static String getLikeKey(int entityType, int entityId){
         return BIZ_LIKE + SPLIT + String.valueOf(entityType) + SPLIT + String.valueOf(entityId);
@@ -29,5 +30,9 @@ public class RedisKeyUtil {
     // 每个用户对某类实体的关注key
     public static String getFolloweeKey(int userId, int entityType){
         return BIZ_FOLLOWEE + SPLIT + String.valueOf(userId) + SPLIT + String.valueOf(entityType);
+    }
+
+    public static String getTimelineKey(int userId) {
+        return BIZ_TIMELINE + SPLIT + String.valueOf(userId);
     }
 }
